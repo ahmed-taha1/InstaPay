05039e62b8ed7eed0b2c7e57c3d6e0030cfbb944
ahmed taha <bjabotaha@yahoo.com>
2023-11-12 21:39:57 +0200

    Bills restored

---
+++ src/Bills/BillInfo.java
@@ -0,0 +1,8 @@
+package Bills;
+
+public abstract class BillInfo {
+    public abstract double CalculateBillCost();
+
+    public double cost;
+    public  int serialNumber;
+}
---
+++ src/Bills/WaterBill.java
@@ -0,0 +1,10 @@
+package Bills;
+
+public class WaterBill extends BillInfo{
+    //    add it's private Data
+
+    @Override
+    public double CalculateBillCost() {
+        return 0;
+    }
+}
