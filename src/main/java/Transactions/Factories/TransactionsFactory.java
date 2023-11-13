73c7c68bb8164185333d95e5c1e984ddd2429398
ahmed taha <bjabotaha@yahoo.com>
2023-11-14 00:35:44 +0200

    transaction factory created

--- src/main/java/Transactions/Factories/TransactionsFactory.java
+++ src/main/java/Transactions/Factories/TransactionsFactory.java
@@ -1,4 +1,30 @@
 package Transactions.Factories;
 
+import Transactions.Entities.ITransaction;
+import Transactions.Entities.TransactionType;
+import Transactions.Entities.TransferToBankAccount;
+import Transactions.Entities.TransferToWallet;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TransactionsFactory {
+    private static TransactionsFactory transactionsFactoryInstance;
+    private static Map<String, ITransaction> transactionMapping;
+    private TransactionsFactory(){
+        transactionMapping = new HashMap<>();
+//        transactionMapping.put(TransactionType.TRANSFER_TO_BANK.toString(),new TransferToBankAccount());
+//        transactionMapping.put(TransactionType.TRANSFER_TO_BANK.toString(),new TransferToWallet());
+//        transactionMapping.put(TransactionType.TRANSFER_TO_BANK.toString(),new TransferToInstapay());
+//        transactionMapping.put(TransactionType.TRANSFER_TO_BANK.toString(),new PayBill());
+    }
+    public static TransactionsFactory getInstance() {
+        if(transactionsFactoryInstance == null){
+            transactionsFactoryInstance = new TransactionsFactory();
+        }
+        return transactionsFactoryInstance;
+    }
+    public ITransaction createTransaction(String transactionType){
+        return transactionMapping.get(transactionType);
+    }
 }
