026c6cfad5270a96564bfab78a0fc71078ca1cba
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2023-11-15 23:53:49 +0200

    final-v

--- src/main/java/InstaPay/SW/spring_boot_instapay_project/Gateways/Factory/PaymentGatewayFactory.java
+++ src/main/java/InstaPay/SW/spring_boot_instapay_project/Gateways/Factory/PaymentGatewayFactory.java
@@ -1,9 +1,14 @@
 package InstaPay.SW.spring_boot_instapay_project.Gateways.Factory;
 
+import InstaPay.SW.spring_boot_instapay_project.Authentication.Exceptions.InvalidCredentials;
+import InstaPay.SW.spring_boot_instapay_project.Authentication.Exceptions.UserNotFound;
 import InstaPay.SW.spring_boot_instapay_project.Gateways.BankGateways.MockGateways.MockBankPaymentGateway;
 import InstaPay.SW.spring_boot_instapay_project.Gateways.IPaymentGateway;
 import InstaPay.SW.spring_boot_instapay_project.Gateways.WalletProviderGateways.MockWalletGateway.MockWalletPayementGateway;
 import InstaPay.SW.spring_boot_instapay_project.Transactions.Entities.ITransaction;
+import InstaPay.SW.spring_boot_instapay_project.Users.Entities.BankUser;
+import InstaPay.SW.spring_boot_instapay_project.Users.Entities.User;
+import InstaPay.SW.spring_boot_instapay_project.Users.Entities.WalletUser;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,17 +28,25 @@ public class PaymentGatewayFactory {
         }
         return paymentGatewayFactoryInstance;
     }
-    public ITransaction createGateway(String gatewayType, Map<String, Object> map){
-        Function<Map<String, Object>, IPaymentGateway> gatewayCreator = gatewayMapping.get(gatewayType);
+    public IPaymentGateway createGateway(String userType, Map<String, Object> map){
+        Function<Map<String, Object>, IPaymentGateway> gatewayCreator = gatewayMapping.get(userType);
         if (gatewayCreator != null) {
-            return (ITransaction) gatewayCreator.apply(map);
+            return  gatewayCreator.apply(map);
         }
         return null;
     }
     private IPaymentGateway createMockBankGateway(Map<String, Object> attr){
-        return new MockBankPaymentGateway((String)attr.get("bankAccount"));
+        if(attr.get("user") == null){
+            return null;
+        }
+        BankUser user = (BankUser) attr.get("user");
+        return new MockBankPaymentGateway(user.getAccountNumber());
     }
     private IPaymentGateway createMockWalletGateway(Map<String, Object> attr){
-        return new MockWalletPayementGateway((String)attr.get("phoneNumber"));
+        if(attr.get("user") == null){
+            return null;
+        }
+        WalletUser user = (WalletUser) attr.get("user");
+        return new MockBankPaymentGateway(user.getPhoneNumber());
     }
 }
--- src/main/java/InstaPay/SW/spring_boot_instapay_project/SpringBootInstapayProjectApplication.java
+++ src/main/java/InstaPay/SW/spring_boot_instapay_project/SpringBootInstapayProjectApplication.java
@@ -1,12 +1,19 @@
 package InstaPay.SW.spring_boot_instapay_project;
 
 import InstaPay.SW.spring_boot_instapay_project.Authentication.Exceptions.UnAuthenticated;
+import InstaPay.SW.spring_boot_instapay_project.Authentication.Exceptions.UnAuthorized;
 import InstaPay.SW.spring_boot_instapay_project.Authentication.Exceptions.UserNotFound;
 import InstaPay.SW.spring_boot_instapay_project.Authentication.TransferAuthorizer;
 import InstaPay.SW.spring_boot_instapay_project.Bills.Entities.BillInfo;
 import InstaPay.SW.spring_boot_instapay_project.Bills.Entities.BillsTypes;
 import InstaPay.SW.spring_boot_instapay_project.Bills.Factories.BillFactory;
+import InstaPay.SW.spring_boot_instapay_project.Gateways.BankGateways.MockGateways.MockBankPaymentGateway;
 import InstaPay.SW.spring_boot_instapay_project.Gateways.Factory.PaymentGatewayFactory;
+import InstaPay.SW.spring_boot_instapay_project.Gateways.IPaymentGateway;
+import InstaPay.SW.spring_boot_instapay_project.Gateways.WalletProviderGateways.MockWalletGateway.MockWalletPayementGateway;
+import InstaPay.SW.spring_boot_instapay_project.Transactions.Entities.ITransaction;
+import InstaPay.SW.spring_boot_instapay_project.Transactions.Entities.TransferMoney;
+import InstaPay.SW.spring_boot_instapay_project.Transactions.Exceptions.InvalidBalance;
 import InstaPay.SW.spring_boot_instapay_project.Users.DataAccess.IUserDataAccess;
 import InstaPay.SW.spring_boot_instapay_project.Users.DataAccess.InMemoryUsers;
 import InstaPay.SW.spring_boot_instapay_project.Users.Entities.BankUser;
@@ -14,8 +21,7 @@ import InstaPay.SW.spring_boot_instapay_project.Users.Entities.User;
 import InstaPay.SW.spring_boot_instapay_project.Users.Entities.WalletUser;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 @SpringBootApplication
 public class SpringBootInstapayProjectApplication {
@@ -30,12 +36,12 @@ public class SpringBootInstapayProjectApplication {
 			}
 		}
 	}
-	private static void startWindow() throws UserNotFound, UnAuthenticated {
+	private static void startWindow() throws UserNotFound, UnAuthenticated, InvalidBalance, UnAuthorized {
 		System.out.println("welcome");
 		System.out.println("please choose option");
 		System.out.println("1- Signup");
 		System.out.println("2- Login");
-		int choose = new Scanner(System.in).nextInt();
+		Stri choose = Integer.parseInt(new Scanner(System.in).nextLine());
 		if(choose == 1){
 			signup();
 		}
@@ -43,8 +49,9 @@ public class SpringBootInstapayProjectApplication {
 			login();
 		}
 	}
-	public static void login() throws UserNotFound, UnAuthenticated {
+	public static void login() throws UserNotFound, UnAuthenticated, InvalidBalance, UnAuthorized {
 		System.out.println("Enter Your Phone Number");
+		new Scanner(System.in).nextLine();
 		String phoneNumber = new Scanner(System.in).nextLine();
 		System.out.println("Enter your userName:");
 		String userName = new Scanner(System.in).nextLine();
@@ -90,7 +97,7 @@ public class SpringBootInstapayProjectApplication {
 			userDataAccess.createUser(new WalletUser(userName, password, phoneNumber, walletProvider));
 		}
 	}
-	public static void transactionWindow(){
+	public static void transactionWindow() throws UserNotFound, UnAuthorized, InvalidBalance {
 		System.out.println("1-Transfer To BankAccount");
 		System.out.println("2-Transfer To WalletUser");
 		System.out.println("3-Transfer To InstaPayUser");
@@ -109,17 +116,24 @@ public class SpringBootInstapayProjectApplication {
 			InquireAboutBalance();
 		}
 	}
-	public static void transferToBankAccount(){
+	public static void transferToBankAccount() throws UserNotFound, UnAuthorized, InvalidBalance {
 		System.out.println("Please Enter Receiver Phone Number:");
 		String phoneNumber = new Scanner(System.in).nextLine();
 		System.out.println("Please Enter The amount to transfer");
 		Double amount = new Scanner(System.in).nextDouble();
 		User receiver = userDataAccess.getUserByMobileNumber(phoneNumber);
 		TransferAuthorizer authorizer = new TransferAuthorizer(activeUser.getUserType(),receiver.getUserType());
-
+		Map<String,Object>senderAttributes = new HashMap<>();
+		senderAttributes.put("user", activeUser);
+		IPaymentGateway senderGateway = PaymentGatewayFactory.getInstance().createGateway(activeUser.getUserType(),senderAttributes);
+		Map<String,Object>recieverAttribures = new HashMap<>();
+		senderAttributes.put("user",receiver);
+		IPaymentGateway recieverGateway = PaymentGatewayFactory.getInstance().createGateway(receiver.getUserType(),recieverAttribures);
+		ITransaction transaction = new TransferMoney(senderGateway,recieverGateway,authorizer);
+		transaction.executeTransaction(amount);
 	}
 
-	public static void transferToWalletUser(){
+	public static void transferToWalletUser()  throws UnAuthorized, InvalidBalance, UserNotFound{
 		System.out.println("please enter receiver phone number");
 		String receiverPhoneNumber = new Scanner(System.in).nextLine();
 
@@ -131,17 +145,33 @@ public class SpringBootInstapayProjectApplication {
 		TransferAuthorizer transferAuthorizer = new TransferAuthorizer(activeUser.getUserType(), receiver.getUserType());
 
 		PaymentGatewayFactory paymentGatewayFactory = PaymentGatewayFactory.getInstance();
-		
 
-//		MockWalletPaymentGateway senderGateway = new MockWalletPaymentGateway(activeUser.getPhoneNumber());
-//        MockWalletPaymentGateway receiverGateway = new MockWalletPaymentGateway(receiverPhoneNumber);
+		Map<String, Object> user1 = new HashMap<>();
+		user1.put("user" ,activeUser);
 
-//		TransferMoney transaction = new TransferMoney(senderGateway, receiverGateway, transferAuthorizer);
-//		transaction.executeTransaction(amount);
+		Map<String, Object> user2 = new HashMap<>();
+		user2.put("user" ,receiver);
+		IPaymentGateway senderPaymentGateway= paymentGatewayFactory.createGateway(activeUser.getUserType(), user1);
+		IPaymentGateway receiverPaymentGateway = paymentGatewayFactory.createGateway(activeUser.getUserType(), user2);
+
+		TransferMoney transaction = new TransferMoney(senderPaymentGateway, receiverPaymentGateway, transferAuthorizer);
+		transaction.executeTransaction(amount);
 	}
 
-	public static void transferToInstaPay(){
-
+	public static void transferToInstaPay() throws UnAuthorized, InvalidBalance, UserNotFound{
+		System.out.println("Please Enter Receiver UserName:");
+		String userName = new Scanner(System.in).nextLine();
+		System.out.println("Please Enter The amount to transfer");
+		Double amount = new Scanner(System.in).nextDouble();
+		User receiver = userDataAccess.getUserByUserName(userName);
+		TransferAuthorizer authorizer = new TransferAuthorizer(activeUser.getUserType(),receiver.getUserType());
+		Map<String, Object>senderAttributes = new HashMap<>();
+		senderAttributes.put("user",activeUser);
+		IPaymentGateway senderGateway = PaymentGatewayFactory.getInstance().createGateway(activeUser.getUserType(),senderAttributes);
+		Map<String,Object>recieverAttribures = new HashMap<>();
+		senderAttributes.put("user",receiver);
+		IPaymentGateway recieverGateway = PaymentGatewayFactory.getInstance().createGateway(receiver.getUserType(),recieverAttribures);
+		ITransaction transaction = new TransferMoney(senderGateway,recieverGateway,authorizer);
 	}
 	public static void payBill(){
 		System.out.println("please choose providor");
@@ -156,6 +186,26 @@ public class SpringBootInstapayProjectApplication {
 		bill.pay();
 	}
 	public static void InquireAboutBalance(){
-
+		Map<String, Object> user1 = new HashMap<>();
+		if(activeUser.getUserType() == "bank"){
+			BankUser user = (BankUser) activeUser;
+			MockBankPaymentGateway mockBankPaymentGateway = new MockBankPaymentGateway(user.getAccountNumber());
+			double amount = 0;
+			try {
+				amount = mockBankPaymentGateway.getBalance();
+			} catch (UserNotFound e) {
+				throw new RuntimeException(e);
+			}
+			System.out.println("your balance is" + amount);
+		}
+		else{
+			MockWalletPayementGateway mockWalletPayementGatey = new MockWalletPayementGateway(activeUser.getPhoneNumber());
+			try {
+				double amount = mockWalletPayementGatey.getBalance();
+				System.out.println("your balance is" + amount);
+			} catch (UserNotFound e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 }
