c0a630f8a0cda56f9a759351c454c738de1e1e04
ahmedhany1284 <ahmedhany4900@gmail.com>
2023-11-15 15:11:28 +0200

    update the user

--- src/main/java/Users/DataAccess/IUserDataAccess.java
+++ src/main/java/Users/DataAccess/IUserDataAccess.java
@@ -5,5 +5,5 @@ import Users.Entities.UserProfile;
 public interface IUserDataAccess {
     public UserProfile getUserByMobileNumber(String phoneNumber);
     public UserProfile getUserByUserName(String Name);
-    public void createUser(UserProfile userProfile);
+//    public void createUser(UserProfile userProfile);
 }
--- src/main/java/Users/Entities/UserProfile.java
+++ src/main/java/Users/Entities/UserProfile.java
@@ -3,15 +3,16 @@ package Users.Entities;
 public class UserProfile {
     private final String userName;
     private final String password;
+    private final String email;
     private final String phoneNumber;
     private final String userType;
-    private boolean isVerified;
-    UserProfile(String userName, String password, String email, String userType, String phoneNumber){
+
+    public UserProfile(String userName, String password, String email, String userType, String phoneNumber){
         this.userName = userName;
         this.password = password;
+        this.email = email;
         this. userType = userType;
         this. phoneNumber = phoneNumber;
-        this.isVerified = false;
     }
     public String getUserName() {
         return userName;
@@ -19,13 +20,23 @@ public class UserProfile {
     public String getPassword() {
         return password;
     }
+    public String getEmail() {
+        return email;
+    }
     public String getPhoneNumber() {
         return phoneNumber;
     }
     public String getUserType() {
         return userType;
     }
-    public void setVerified(boolean verified) {
-        isVerified = verified;
+
+    public Object getUserEntity(String number) {
+        if (userType=="BankUser") {
+            return new BankUser(this, number);
+        } else if (userType=="WalletUser") {
+            return new WalletUser(this);
+        } else {
+            return ("Invalid user type");
+        }
     }
 }
