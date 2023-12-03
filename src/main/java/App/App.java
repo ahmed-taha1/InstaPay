8db727f281dea6cfeed17c2c9c0e07dfa2c11a55
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2023-12-03 14:56:58 +0200

    Renamed Server class

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -1,9 +1,9 @@
-package server;
+package App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class ApiApplication {
+public class App {
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 }
